827c2e652252ba868bbe7dda629d3242240d2f90
Izpodvypodvert <albukerke87@mail.ru>
2023-11-23 21:52:27 +0300

    Added celery task, smtp is configured

--- bibliomanager/api/views.py
+++ bibliomanager/api/views.py
@@ -3,12 +3,28 @@ from rest_framework.viewsets import ModelViewSet
 
 from books.models import Book
 from .serializers import BookSerializer, UserRegistrationSerializer
+from .tasks import send_welcome_email
 
 
 class BookViewSet(ModelViewSet):
+    """
+    Представление для просмотра и редактирования книг.
+
+    Это представление позволяет получить список всех книг, создать новую книгу,
+    получить детали конкретной книги, обновить или удалить книгу.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class UserRegistrationView(CreateAPIView):
+    """
+    Представление для регистрации нового пользователя.
+
+    При успешной регистрации отправляет пользователю приветственное письмо.
+    """
     serializer_class = UserRegistrationSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        send_welcome_email.delay(user.email)
---
+++ bibliomanager/bibliomanager/celery_config.py
@@ -0,0 +1,10 @@
+import os
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliomanager.settings')
+
+app = Celery('bibliomanager')
+app.conf.broker_connection_retry_on_startup = True
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
